f60f8ddfb4354b4b4d1af8661e5dc43470152f5d
SupathiRatnayake <r.supathi@gmail.com>
2020-03-14 10:31:32 +0530

    optimize imports

--- src/bookingApp/Main.java
+++ src/bookingApp/Main.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
@@ -13,10 +12,12 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
-import java.util.*;
+import java.util.LinkedHashMap;
+import java.util.Optional;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Main extends Application {
 
